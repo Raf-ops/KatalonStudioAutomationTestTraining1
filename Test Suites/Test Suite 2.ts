d1639bfa5e6c75e220655ad46dd134e42fe35673
Raf-ops <60862293+Raf-ops@users.noreply.github.com>
2021-02-23 16:33:52 +0700

    create test_1 & Test_2 and Test Suite 2

---
+++ Test Suites/Test Suite 2.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cd4d083a-bc9c-4e92-94eb-0ea5ffdc6482</testSuiteGuid>
+   <testCaseLink>
+      <guid>776c1c73-51ae-453c-a0e8-c5a87120ae11</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d0b6c286-ebbb-4847-980c-ee5961669993</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
